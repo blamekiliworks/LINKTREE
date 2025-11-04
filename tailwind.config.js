3eb986a141c71ea15b4f5e45a591a9d36a2b6541
blamekiliworks <blamekiliworks@gmail.com>
2025-11-04 19:35:19 +0100

    Updated index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Orbitron', 'sans-serif'],
+        mono: ['Orbitron', 'monospace'],
+      },
+    },
   },
   plugins: [],
 };
